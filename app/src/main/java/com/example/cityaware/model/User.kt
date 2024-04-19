0d235b6229eb0e8726536bc432d168a5d23220cb
DorSonego <dorsonego123@gmail.com>
2024-04-19 14:52:24 +0300

    update

---
+++ app/src/main/java/com/example/cityaware/model/User.kt
@@ -0,0 +1,32 @@
+package com.example.karenhub.model
+
+class User {
+    var email: String? = ""
+    var label: String? = ""
+
+    internal constructor()
+    constructor(email: String?, label: String?) {
+        this.email = email
+        this.label = label
+    }
+
+    fun toJson(): Map<String, Any?> {
+        val json: MutableMap<String, Any?> = HashMap()
+        json[EMAIL] = email
+        json[ACCOUNT_LABEL] = label
+        return json
+    }
+
+    companion object {
+        const val EMAIL = "email"
+        const val ACCOUNT_LABEL = "label"
+        const val COLLECTION = "users"
+        fun fromJson(json: Map<String?, Any?>): User {
+            val email =
+                json[EMAIL] as String?
+            val label =
+                json[ACCOUNT_LABEL] as String?
+            return User(email, label)
+        }
+    }
+}
