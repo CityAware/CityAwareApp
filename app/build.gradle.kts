d50374b4f88516671bcb246d6fc71ec820bdfec4
DorSonego <dorsonego123@gmail.com>
2024-04-19 14:51:39 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.room.common)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
