fc4770ee35c69d7dd5300d5e3ea94dcb8048230e
DorSonego <dorsonego123@gmail.com>
2024-04-20 19:58:46 +0300

    fix login and add auth

--- app/src/main/java/com/example/cityaware/AddNewPostFragment.kt
+++ app/src/main/java/com/example/cityaware/AddNewPostFragment.kt
@@ -64,6 +64,7 @@ class AddNewPostFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+        //
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentAddPostBinding.inflate(inflater, container, false)
